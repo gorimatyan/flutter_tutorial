f04218991149243d31a6ef81657890be80a1202a
gorimatyan <gorima102131@gmail.com>
2023-10-02 02:31:06 +0900

    section: StateProviderの使い方、Providerとの違いを知った。

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  // アプリ全体でriverpodの状態を共有するためのProviderを使う
+  runApp(const ProviderScope(child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
@@ -16,35 +18,31 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+// 状態が変化する必要があるならStateProvider,変化しないならProviderを使う。
+final StateProvider<int> appCount =
+    StateProvider<int>((StateProviderRef<int> ref) {
+  return 3;
+});
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class MyHomePage extends ConsumerWidget {
+  const MyHomePage({
+    super.key,
+  });
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // ref.watch(変数名)でStateProviderの値を受け取れる。
+    final int count = ref.watch(appCount);
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Riverpod App'),
       ),
       body: Center(
         child: Column(
@@ -54,14 +52,17 @@ class _MyHomePageState extends State<MyHomePage> {
               'フラッターアプリです！！',
             ),
             Text(
-              '$_counter',
+              count.toString(),
               style: Theme.of(context).textTheme.headlineMedium,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          // ref.readでStateProviderの値を呼び出す
+          ref.read(appCount.notifier).state++;
+        },
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
